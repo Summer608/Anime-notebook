a0c6ce05b5cfbf1cf465410f03476fb075e8eb06
Summer608 <2331821297@qq.com>
2026-08-24 13:50:41 +0800

    refactor: 来自深渊季数改用namedSeasons数组格式

--- src/data/animeKnowledge.ts
+++ src/data/animeKnowledge.ts
@@ -439,11 +439,10 @@ export const animeKnowledgeBase: AnimeKnowledge[] = [
     aliases: ["来自深渊"],
     fullName: "来自深渊 / メイドインアビス",
     genres: ["奇幻", "冒险", "恐怖", "治愈", "战斗"],
-    seasons: [1, 2, 3],
-    namedSeasons: {
-      2: "来自深渊 深沉灵魂的黎明",
-      3: "来自深渊 烈日的黄金乡",
-    },
+    namedSeasons: [
+      { suffix: "深沉灵魂的黎明", label: "深沉灵魂的黎明" },
+      { suffix: "烈日的黄金乡", label: "烈日的黄金乡" },
+    ],
   },
   {
     aliases: ["魔法少女小圆", "小圆"],
